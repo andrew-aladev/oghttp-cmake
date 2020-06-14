eb3b4a784e8cc6b98124af4aba06c35b835714a8
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-06-14 22:45:45 +0300

    added ability to use same toolchain for generators (testing)

--- generators/common/toolchain.cmake
+++ generators/common/toolchain.cmake
@@ -1,3 +1,5 @@
-include ("${CMAKE_CURRENT_LIST_DIR}/../../toolchains/default.cmake")
+if (NOT CMAKE_GENERATOR_USE_CURRENT_TOOLCHAIN)
+  include ("${CMAKE_CURRENT_LIST_DIR}/../../toolchains/default.cmake")
+endif ()
 
 set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O2")
