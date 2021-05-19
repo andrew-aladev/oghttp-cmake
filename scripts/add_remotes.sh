2cb210fedf415bbd5e94ff22301de7ac5d913bed
Andrew Aladjev <aladjev.andrew@gmail.com>
2021-05-20 00:44:54 +0300

    cosmetics

--- scripts/add_remotes.sh
+++ scripts/add_remotes.sh
@@ -4,6 +4,6 @@ set -e
 DIR=$(dirname "${BASH_SOURCE[0]}")
 cd "$DIR"
 
-git remote add github    "git@github.com:andrew-aladev/oghttp-cmake.git"    || true
-git remote add bitbucket "git@bitbucket.org:andrew-aladev/oghttp-cmake.git" || true
-git remote add gitlab    "git@gitlab.com:andrew-aladev/oghttp-cmake.git"    || true
+git remote add github    "git@github.com:andrew-aladev/oghttp-cmake.git"    || :
+git remote add bitbucket "git@bitbucket.org:andrew-aladev/oghttp-cmake.git" || :
+git remote add gitlab    "git@gitlab.com:andrew-aladev/oghttp-cmake.git"    || :
--- scripts/update.sh
+++ scripts/update.sh
@@ -4,8 +4,8 @@ set -e
 DIR=$(dirname "${BASH_SOURCE[0]}")
 cd "$DIR"
 
-git fetch --all || true
-git fetch --tags || true
-git remote | xargs -I {} git rebase "{}/$(git branch --show-current)" || true
+git fetch --all || :
+git fetch --tags || :
+git remote | xargs -I {} git rebase "{}/$(git branch --show-current)" || :
 
 ./registry/update.sh
